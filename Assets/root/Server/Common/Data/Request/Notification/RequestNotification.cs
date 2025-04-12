b011dc90c0762cdc16463348d0b2bcb3dc443508
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2025-04-12 01:37:59 -0700

    refactor: Implement IRequestID in IRequestNotification and update RequestNotification constructor for improved request handling

--- Assets/root/Server/Common/Data/Request/Notification/RequestNotification.cs
+++ Assets/root/Server/Common/Data/Request/Notification/RequestNotification.cs
@@ -1,19 +1,24 @@
 #pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
+using System;
 using System.Collections.Generic;
 
 namespace com.IvanMurzak.Unity.MCP.Common.Data
 {
     public class RequestNotification : IRequestNotification
     {
+        public string RequestID { get; set; } = string.Empty;
         public string? Path { get; set; }
         public string? Name { get; set; }
         public IDictionary<string, object?>? Parameters { get; set; } = new Dictionary<string, object?>();
 
         public RequestNotification() { }
-        public RequestNotification(string path, string name) : this()
+        public RequestNotification(string path, string name)
+        : this(Guid.NewGuid().ToString(), path, name) { }
+        public RequestNotification(string requestId, string path, string name) : this()
         {
-            Path = path;
-            Name = name;
+            RequestID = requestId ?? throw new ArgumentNullException(nameof(requestId));
+            Path = path ?? throw new ArgumentNullException(nameof(path));
+            Name = name ?? throw new ArgumentNullException(nameof(name));
         }
 
         public virtual void Dispose()
