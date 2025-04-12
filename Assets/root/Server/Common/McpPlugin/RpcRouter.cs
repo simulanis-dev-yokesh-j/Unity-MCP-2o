c5100f2bd8d609fd7c4d841780ed362d5d776387
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2025-04-12 16:43:27 -0700

    refactor: Remove unused using directive for System.Collections.Generic in RpcRouter

--- Assets/root/Server/Common/McpPlugin/RpcRouter.cs
+++ Assets/root/Server/Common/McpPlugin/RpcRouter.cs
@@ -1,6 +1,5 @@
 #pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
 using System;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using com.IvanMurzak.Unity.MCP.Common.Data;
