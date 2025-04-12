e4b44d9c055e19b95f3c42cc47701b8c4fc204cf
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2025-04-12 13:01:13 -0700

    feat: Enable Kestrel server with IP listening for improved connection handling

--- Assets/root/Server/Program.cs
+++ Assets/root/Server/Program.cs
@@ -75,10 +75,10 @@ namespace com.IvanMurzak.Unity.MCP.Server
                 // TODO: add reading from configs (json file and env variables)
                 // "http://localhost:60606");
                 builder.WebHost.UseUrls(Consts.Hub.DefaultEndpoint);
-                // builder.WebHost.UseKestrel(options =>
-                // {
-                //     options.ListenAnyIP(60606); // TODO: add reading from configs (json file and env variables)
-                // });
+                builder.WebHost.UseKestrel(options =>
+                {
+                    options.ListenAnyIP(60606); // TODO: add reading from configs (json file and env variables)
+                });
 
                 var app = builder.Build();
 
