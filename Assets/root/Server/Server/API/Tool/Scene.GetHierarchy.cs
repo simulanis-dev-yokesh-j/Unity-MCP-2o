4793fe06b93821b52f13be0d79b1b93b2911a7a4
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2025-04-13 20:01:02 -0700

    Added Scene.GetHierarchy

---
+++ Assets/root/Server/Server/API/Tool/Scene.GetHierarchy.cs
@@ -0,0 +1,34 @@
+using ModelContextProtocol.Protocol.Types;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace com.IvanMurzak.Unity.MCP.Server.API
+{
+    public partial class Tool_Scene
+    {
+        [McpServerTool
+        (
+            Name = "GameObject_FindByPath",
+            Title = "Find GameObject by path"
+        )]
+        [Description("Find GameObject tree by the path to the root GameObject. Returns metadata about each GameObject.")]
+        public Task<CallToolResponse> FindByPath
+        (
+            [Description("Path to the GameObject.")]
+            string path,
+            [Description("Include children GameObjects in the result.")]
+            bool includeChildren = true,
+            [Description("Include children GameObjects recursively in the result. Ignored if 'includeChildren' is false.")]
+            bool includeChildrenRecursively = false
+        )
+        {
+            return ToolRouter.Call("GameObject_FindByPath", arguments =>
+            {
+                arguments[nameof(path)] = path;
+                arguments[nameof(includeChildren)] = includeChildren;
+                arguments[nameof(includeChildrenRecursively)] = includeChildrenRecursively;
+            });
+        }
+    }
+}
